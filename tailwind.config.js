1370bb0e8e71446b8240c9e4611214ab200611e6
Nandika <dnsw83@gmail.com>
2025-09-06 15:08:04 +0530

    buid issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.css', // Add this line
   ],
   theme: {
     extend: {
@@ -9,6 +10,7 @@ module.exports = {
         foreground: '#F5F5F7', // Soft off-white for text
         ivory: '#232325',      // Muted dark for cards
         ash: '#2C2C2E',        // Ashy gray for borders
+        orange: '#FF7A00',
         secondary: '#FF7A00'
       },
     },
